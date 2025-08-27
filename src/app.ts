93114e771158adf212491113b77b187ce5a635c0
kartikey sharma <kartikey7518@gmail.com>
2025-08-27 22:51:35 +0530

    origin issue

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: ["http://localhost:5173", "https://second-brain-eta-five.vercel.app/"], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
