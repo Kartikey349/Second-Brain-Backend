e137ed9b970ff49a80e1c73ac60a375388b6e14c
kartikey sharma <kartikey7518@gmail.com>
2025-08-27 23:12:58 +0530

    _

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -72,7 +72,10 @@ userRouter.post("/login", async (req,res) => {
             const token = jwt.sign({id: user._id}, "SecondBrain", {
                 expiresIn: "1d"
             });
-            res.cookie("token", token)
+            res.cookie("token", token, {httpOnly: true,
+            secure: true, // only https
+            sameSite: "none",
+            maxAge: 24*60*60*1000} )
         }
 
         res.send(user)
